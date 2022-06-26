1ce252e3f09fd1123ea8fc26885a25678d0cf87d
tamari.mumladze@paysera.net <tamunamumladze777@gmail.com>
2022-06-26 21:58:45 +0400

    add migrations

---
+++ Lennt/Lennt.Model/Migrations/20220626175643_Add vanacyPersons table.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lennt.Model.Migrations
+{
+    public partial class AddvanacyPersonstable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
